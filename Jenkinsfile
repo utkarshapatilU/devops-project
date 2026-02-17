75e66d838d8642519675dbe569a96be7f8970b3d
Utkarsha patil <patilur010@gmail.com>
2026-02-17 11:13:41 +0530

    updated files

--- Jenkinsfile
+++ Jenkinsfile
@@ -12,12 +12,11 @@ pipeline {
 
         stage('Terraform Init & Apply') {
             steps {
-                withCredentials([[
+                withCredentials([[ 
                     $class: 'AmazonWebServicesCredentialsBinding',
                     credentialsId: 'jenkins-creds'
                 ]]) {
                     dir('Terraform') {
-                        // Clean old plugins/cache to avoid timeout
                         sh 'rm -rf .terraform && rm -f .terraform.lock.hcl'
                         sh 'terraform init -upgrade'
                         sh 'terraform apply -auto-approve'
@@ -26,25 +25,5 @@ pipeline {
             }
         }
 
-        stage('Deploy App with Ansible') {
-            steps {
-                dir('Ansible') {
-                    script {
-                        // Get EC2 public IP from Terraform outputs
-                        def ec2_ip = sh(
-                            script: "terraform -chdir=../Terraform output -raw ec2_public_ip",
-                            returnStdout: true
-                        ).trim()
-
-                        // Generate inventory dynamically
-                        sh "echo '[servers]' > inventory.ini"
-                        sh "echo '${ec2_ip} ansible_user=ubuntu ansible_ssh_private_key_file=../Terraform/jenkins-key.pem' >> inventory.ini"
-
-                        // Run Ansible playbook
-                        sh 'ansible-playbook -i inventory.ini playbook.yml'
-                    }
-                }
-            }
-        }
     }
 }
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,24 +13,35 @@ resource "aws_instance" "server" {
   }
 }
 
-resource "null_resource" "ansible_run" {
+# -------- WAIT FOR SSH --------
+resource "null_resource" "wait_for_ssh" {
   depends_on = [aws_instance.server]
 
-  connection {
-    type        = "ssh"
-    host        = aws_instance.server.public_ip
-    user        = "ubuntu"
-    private_key = file(var.ssh_private_key_path)
-    timeout     = "5m"
-  }
+  provisioner "remote-exec" {
+    inline = ["echo 'EC2 is ready for SSH'"]
 
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "[server]" > ../Ansible/inventory.ini
-      echo "${aws_instance.server.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${var.ssh_private_key_path}" >> ../Ansible/inventory.ini
-      ansible-playbook -i ../Ansible/inventory.ini ../Ansible/playbook.yml
-    EOT
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = aws_instance.server.public_ip
+      private_key = file(var.ssh_private_key_path)
+      timeout     = "10m"
+    }
   }
 }
 
+# -------- ANSIBLE RUN --------
+resource "null_resource" "ansible_run" {
+  depends_on = [null_resource.wait_for_ssh]
 
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "[servers]" > ../Ansible/inventory.ini
+      echo "${aws_instance.server.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${var.ssh_private_key_path}" >> ../Ansible/inventory.ini
+
+      ANSIBLE_HOST_KEY_CHECKING=False \
+      ansible-playbook -i ../Ansible/inventory.ini ../Ansible/playbook.yml \
+      --ssh-extra-args="-o StrictHostKeyChecking=no"
+    EOT
+  }
+}
